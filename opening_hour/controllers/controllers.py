16b10e0cceb323d299f3877f72d00066498e952b
hprasetyou <hprasetyou@gmail.com>
2018-03-27 21:04:59 +0700

    remove sameday validation

--- opening_hour/controllers/controllers.py
+++ opening_hour/controllers/controllers.py
@@ -1,71 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
-import datetime
+import datetime,pytz
+import json
 
 class OpeningHour(http.Controller):
-    @http.route('/opening_hour/opening_hour/', type='json', auth='user')
-    def index(self, **kw):
-        event = 'Bussiness is open'
-        calendar = request.env['calendar.event']
-        days = ['mon','tue','wed','thu','fri','sat','sun']
-        today = days[datetime.datetime.today().weekday()]
-        opening_hour = request.env['opening_hour.opening_hour']
-        oh_reg = opening_hour.search([('day','=',str(today))])
-        oh_special = opening_hour.search([('day','=',datetime.date.today())])
-        check_bio = calendar.search([('name','=',event)])
-        bussines_time = {
-            'opening':0,
-            'closing':0
-        }
-        if not check_bio:
-            if oh_special:
-                bussines_time = {
-                    'opening':oh_special.opening_hour,
-                    'closing':oh_special.closing_hour
-                }
-            else:
-                if oh_reg:
-                    bussines_time = {
-                        'opening':oh_reg.opening_hour,
-                        'closing':oh_reg.closing_hour
-                    }
-            if bussines_time['opening']>0:
-                print self.create_time(bussines_time['opening'])
-                print self.create_time(bussines_time['closing'])
-                opening = calendar.create({
-                    'name':event,
-                    'start':self.create_time(bussines_time['opening']),
-                    'stop':self.create_time(bussines_time['closing'])
-                })
+    @http.route('/opening_hour/opening_hour/', type='http', auth='public')
+    def get_opening_hours(self, **kw):
+        event = request.env['calendar.event']
+        b_hours = event.search([
+        ('bussiness_time_event','=',True),
+        ('start','>',self.utc_time(datetime.datetime.now().strftime("%Y-%m-%d 00:00:01"))),
+        ('start','<',self.utc_time(datetime.datetime.now().strftime("%Y-%m-%d 23:59:59")))
+        ])
+        for b_hour in b_hours:
+            print b_hour.start
+        return {'status':'ok'}
 
-        return {"status":"ok"}
-
-
-    def create_time(self, hour):
-        now = datetime.datetime.now()
-        curdate = now.strftime("%Y-%m-%d")
-        tm = int(hour)-7
-        if tm<10:
-            tm = '0'+str(tm)
-        mnt = int((hour % 1)*60)
-        if mnt>59:
-            mnt = 59
-        if mnt < 10:
-            mnt = '0'+str(mnt)
-        return '%s %s:%s:00' % (curdate,tm,mnt)
-        
-
-
-#     @http.route('/opening_hour/opening_hour/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('opening_hour.listing', {
-#             'root': '/opening_hour/opening_hour',
-#             'objects': http.request.env['opening_hour.opening_hour'].search([]),
-#         })
-
-#     @http.route('/opening_hour/opening_hour/objects/<model("opening_hour.opening_hour"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('opening_hour.object', {
-#             'object': obj
-#         })
+    def utc_time(self,i_dt):
+        local = pytz.timezone ("Asia/Jakarta")
+        naive = datetime.datetime.strptime (i_dt, "%Y-%m-%d %H:%M:%S")
+        local_dt = local.localize(naive, is_dst=None)
+        utc_dt = local_dt.astimezone (pytz.utc)
+        return utc_dt.strftime("%Y-%m-%d %H:%M:%S")
--- opening_hour/models/opening_hour.py
+++ opening_hour/models/opening_hour.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, exceptions, _
+import datetime,pytz
 
 class OpeningHour(models.Model):
     _name = 'opening_hour.opening_hour'
@@ -16,19 +17,60 @@ class OpeningHour(models.Model):
     ('sat', 'Saturday')])
     opening_hour = fields.Float(text='Opening Hour')
     closing_hour = fields.Float(text='Closing Hour')
-    date = fields.Date(attrs={'invisible'})
 
     @api.model
     def create(self, values):
         if values['day'] != 'date':
-            oh = self.env['opening_hour.opening_hour'].search([('day','=',values['day'])])
-            if oh:
-                raise exceptions.ValidationError(_("Rule already exist!"))
+            bussiness_times = self.env['opening_hour.opening_hour'].search([('day','=',values['day'])])
+            for bussiness_time in bussiness_times:
+                if bussiness_time.closing_hour > values['opening_hour']:
+                    raise exceptions.ValidationError(_("Opening hour must be bigger than closing hour"))
+
         return super(OpeningHour, self).create(values)
-        
-    # @api.onchange('day')
-    # def onchange_day(self):
-    #     if not self.name:
-    #         self.name = self.day
 
+    @api.multi
+    def set_opening_hour(self):
+        event = 'Bussiness is open'
+        calendar = self.env['calendar.event']
+        days = ['mon','tue','wed','thu','fri','sat','sun']
+        today = days[datetime.datetime.today().weekday()]
+        opening_hour = self.env['opening_hour.opening_hour']
+        bt_regs = opening_hour.search([('day','=',str(today))])
+        curdate = datetime.datetime.now().strftime("%Y-%m-%d")
+        check_bio = calendar.search([('name','=',event)])
+        for bussiness_time in bt_regs:
+            event_exist = calendar.search([('name','=',event),
+            ('bussiness_time_event','=',True),
+            ('start','=',self.create_time(bussiness_time.opening_hour)),
+            ('stop','=',self.create_time(bussiness_time.closing_hour))])
+            if not event_exist:
+                new_event = calendar.create({
+                    'name':event,
+                    'bussiness_time_event':True,
+                    'start':self.create_time(bussiness_time.opening_hour),
+                    'stop':self.create_time(bussiness_time.closing_hour)
+                })
+        return {"status":"ok"}
 
+    def create_time(self, hour):
+        now = datetime.datetime.now()
+        curdate = now.strftime("%Y-%m-%d")
+        tm = int(hour)
+        if tm<10:
+            tm = '0'+str(tm)
+        mnt = int((hour % 1)*60)
+        if mnt>59:
+            mnt = 59
+        if mnt < 10:
+            mnt = '0'+str(mnt)
+        local = pytz.timezone ("Asia/Jakarta")
+        o_dt = '%s %s:%s:00' % (curdate,tm,mnt)
+        naive = datetime.datetime.strptime (o_dt, "%Y-%m-%d %H:%M:%S")
+        local_dt = local.localize(naive, is_dst=None)
+        utc_dt = local_dt.astimezone (pytz.utc)
+        return utc_dt.strftime("%Y-%m-%d %H:%M:%S")
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    bussiness_time_event = fields.Boolean('Bussiness Time Event')
